e24c9ecbcc72b227eb874627eacc400161da690f
SuperSonic <supersonic@livemail.tw>
2023-05-11 05:23:32 +0800

    reset

--- internal/Config/rethink.go
+++
@@ -1,9 +0,0 @@
-// xi.recv - The opensource direct message service for LINE OpenChat (LINE Square).
-// License: Apache License 2.0
-// (c) 2021 Star Inc. and its contributors.
-
-package Config
-
-const (
-	EnvRethinkdbAddr = "XI_RETHINKDB_ADDR"
-)
--- internal/Service/Web/Hook/user.go
+++
@@ -1,5 +0,0 @@
-// xi.recv - The opensource direct message service for LINE OpenChat (LINE Square).
-// License: Apache License 2.0
-// (c) 2021 Star Inc. and its contributors.
-
-package Hook
